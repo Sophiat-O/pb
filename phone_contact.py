890378f1110f4c7fa347cab1d65749f2ba02fa61
Sophiat-O <lilsylph@gmail.com>
2020-06-28 20:37:44 +0100

    add git ignore

--- phone_contact.py
+++ phone_contact.py
@@ -2,13 +2,13 @@ import sqlite3
 from sqlite3 import Error
 
 def create_connection(db_file):
-    conn = None
+    connection = None
     try:
-        conn = sqlite3.connect(db_file)
+        connection = sqlite3.connect(db_file)
     except Error as e:
         print(e)
 
-    return conn
+    return connection
 
 def create_table(conn, create_table_sql):
     try:
